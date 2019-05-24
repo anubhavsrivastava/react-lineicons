e5934c9b93dc9f377dc24ffc5dc51225db251d9b
Anubhav Srivastava <anubhav.srivastava00@gmail.com>
2019-05-24 13:38:43 +0530

    fixed babel loader config

--- webpack.config.js
+++ webpack.config.js
@@ -11,13 +11,13 @@ module.exports = {
 		rules: [
 			{
 				test: /\.js$/,
-				include: path.resolve(__dirname, 'src'),
+				// include: path.resolve(__dirname, 'src'),
 				exclude: /(node_modules|bower_components|build)/,
 				use: {
-					loader: 'babel-loader',
-					options: {
-						presets: ['@babel/env']
-					}
+					loader: 'babel-loader'
+					// options: {
+					// 	presets: ['@babel/preset-env', '@babel/preset-react']
+					// }
 				}
 			},
 			{
