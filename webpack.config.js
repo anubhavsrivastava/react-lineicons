bdf240d1c05bf79377b781e15dd51fb7a2912735
Anubhav Srivastava <anubhav.srivastava00@gmail.com>
2019-05-24 14:43:31 +0530

    updated src path

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 var path = require('path');
 module.exports = {
 	mode: 'production',
-	entry: './index.js',
+	entry: './src/index.js',
 	output: {
 		path: path.resolve(__dirname, 'dist'),
 		filename: 'index.js',
