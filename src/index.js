2cd4e639c1712d723d2d8e22ca9d12e4dc9c7981
anubhavsrivastava <anubhav.srivastava00@gmail.com>
2020-03-18 21:11:20 +0100

    feat : added new version of line-icon

--- src/index.js
+++ src/index.js
@@ -1,26 +1,55 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import '../line-icons/LineIcons.css';
+import React from "react";
+import PropTypes from "prop-types";
+import "../line-icons/font-css/LineIcons.css";
+import "../line-icons/override.css";
 
 const LineIcon = ({ name, size, effect, style, tag: Tag, ...rest }) => {
-	return <Tag className={`lni-${name} size-${size} ${effect ? `lni-${effect}-effect` : ''}`} style={style} {...rest} />;
+  return (
+    <Tag
+      className={`lni lni-${name} size-${size} ${
+        effect ? `lni-${effect}-effect` : ""
+      }`}
+      style={style}
+      {...rest}
+    />
+  );
 };
 
-const tagPropType = PropTypes.oneOfType([PropTypes.func, PropTypes.string, PropTypes.shape({ $$typeof: PropTypes.symbol, render: PropTypes.func }), PropTypes.arrayOf(PropTypes.oneOfType([PropTypes.func, PropTypes.string, PropTypes.shape({ $$typeof: PropTypes.symbol, render: PropTypes.func })]))]);
+const tagPropType = PropTypes.oneOfType([
+  PropTypes.func,
+  PropTypes.string,
+  PropTypes.shape({ $$typeof: PropTypes.symbol, render: PropTypes.func }),
+  PropTypes.arrayOf(
+    PropTypes.oneOfType([
+      PropTypes.func,
+      PropTypes.string,
+      PropTypes.shape({ $$typeof: PropTypes.symbol, render: PropTypes.func })
+    ])
+  )
+]);
 
 LineIcon.propTypes = {
-	tag: tagPropType,
-	name: PropTypes.string.isRequired,
-	size: PropTypes.oneOf(['lg', 'md', 'sm', 'xs']),
-	effect: PropTypes.oneOf(['spin', 'tada', 'flashing', 'burst', 'fade-left', 'fade-right', 'fade-up', 'fade-down']),
-	style: PropTypes.object
+  tag: tagPropType,
+  name: PropTypes.string.isRequired,
+  size: PropTypes.oneOf(["lg", "md", "sm", "xs"]),
+  effect: PropTypes.oneOf([
+    "spin",
+    "tada",
+    "flashing",
+    "burst",
+    "fade-left",
+    "fade-right",
+    "fade-up",
+    "fade-down"
+  ]),
+  style: PropTypes.object
 };
 
 LineIcon.defaultProps = {
-	tag: 'i',
-	size: 'md',
-	style: {},
-	effect: null
+  tag: "i",
+  size: "md",
+  style: {},
+  effect: null
 };
 
 export default LineIcon;
